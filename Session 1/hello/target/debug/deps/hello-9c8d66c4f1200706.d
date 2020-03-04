3a8498a2e4f4f26aac940969bb6cc79f0be0cb4d
Sahand Raeisi <sahandraeisi1994@gmail.com>
2020-03-04 14:51:53 +0330

    Create hello with cargo build

---
+++ Session 1/hello/target/debug/deps/hello-9c8d66c4f1200706.d
@@ -0,0 +1,5 @@
+/Users/home/Documents/SHNDProjects/Rust/Session 1/hello/target/debug/deps/hello-9c8d66c4f1200706: src/main.rs
+
+/Users/home/Documents/SHNDProjects/Rust/Session 1/hello/target/debug/deps/hello-9c8d66c4f1200706.d: src/main.rs
+
+src/main.rs:
---
+++ Session 1/hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/Users/home/Documents/SHNDProjects/Rust/Session\ 1/hello/target/debug/hello: /Users/home/Documents/SHNDProjects/Rust/Session\ 1/hello/src/main.rs
